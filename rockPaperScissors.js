d2cd543c49e4e170d881f42d97fc06c045c8ab17
birdpersonn <chanelcarp@gmail.com>
2022-12-06 12:00:00 -0800

    add function to play 5 rounds and display results

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,3 +1,6 @@
+/*
+    desc: returns a random choice of either rock, paper, or scissors
+*/
 function getComputerChoice() {
     switch(Math.floor(Math.random() * 3)) {
         case 0:
@@ -12,6 +15,9 @@ function getComputerChoice() {
     }
 }
 
+/*
+    desc: returns a string indicating whether the player won or lost that round
+*/
 function playRound(playerSelection, computerSelection) {
     const playerChoice = playerSelection.toLowerCase();
     const computerChoice = computerSelection.toLowerCase();
@@ -20,31 +26,50 @@ function playRound(playerSelection, computerSelection) {
         return "you tied!";
     } else if (playerChoice === "rock") {
         if (computerChoice === "paper") {
-            return `you lose! ${compChoice} beats ${playerChoice}`;
+            return `you lose! ${computerChoice} beats ${playerChoice}`;
         } else {
-            return `you win! ${playerChoice} beats ${compChoice}`;
+            return `you win! ${playerChoice} beats ${computerChoice}`;
         }
     } else if (playerChoice == "paper") {
         if (computerChoice === "rock") {
-            return `you win! ${playerChoice} beats ${compChoice}`;
+            return `you win! ${playerChoice} beats ${computerChoice}`;
         } else {
-            return `you lose! ${compChoice} beats ${playerChoice}`;
+            return `you lose! ${computerChoice} beats ${playerChoice}`;
         }
     } else {
         if(computerChoice === "rock") {
-            return `you lose! ${compChoice} beats ${playerChoice}`;
+            return `you lose! ${computerChoice} beats ${playerChoice}`;
         } else {
-            return `you win! ${playerChoice} beats ${compChoice}`;
+            return `you win! ${playerChoice} beats ${computerChoice}`;
         }
     }
 }
 
+/*
+    desc: plays 5 rounds and displays the game winner and scores
+*/
 function game() {
-    for(let i = 0; i < 5; i++) {
+    let playerScore = 0;
+    let compScore = 0;
 
+    for(let i = 0; i < 5; i++) {
+        const playerChoice = "scissors";
+        const compChoice = getComputerChoice();
+        const roundResult = playRound(playerChoice, compChoice);
+        console.log(roundResult);
+        if (roundResult.includes("win")) {
+            playerScore++;
+        } else if (roundResult.includes("lose")) {
+            compScore++;
+        } else {
+            // round is tied, do not include round in total 5
+            i--;
+        }
     }
+
+    console.log(`player score: ${playerScore}`);
+    console.log(`computer score: ${compScore}`);
 }
 
-compChoice = getComputerChoice();
-console.log(compChoice);
-console.log(playRound("ROCk", compChoice));
+
+game();
