e85dad5ef38694bb929fd4ae06ba7699767aca73
birdpersonn <chanelcarp@gmail.com>
2022-12-06 11:51:52 -0800

    build function to simulate 1 round of game

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -12,4 +12,39 @@ function getComputerChoice() {
     }
 }
 
-console.log(getComputerChoice())
+function playRound(playerSelection, computerSelection) {
+    const playerChoice = playerSelection.toLowerCase();
+    const computerChoice = computerSelection.toLowerCase();
+
+    if (playerChoice === computerChoice) {
+        return "you tied!";
+    } else if (playerChoice === "rock") {
+        if (computerChoice === "paper") {
+            return `you lose! ${compChoice} beats ${playerChoice}`;
+        } else {
+            return `you win! ${playerChoice} beats ${compChoice}`;
+        }
+    } else if (playerChoice == "paper") {
+        if (computerChoice === "rock") {
+            return `you win! ${playerChoice} beats ${compChoice}`;
+        } else {
+            return `you lose! ${compChoice} beats ${playerChoice}`;
+        }
+    } else {
+        if(computerChoice === "rock") {
+            return `you lose! ${compChoice} beats ${playerChoice}`;
+        } else {
+            return `you win! ${playerChoice} beats ${compChoice}`;
+        }
+    }
+}
+
+function game() {
+    for(let i = 0; i < 5; i++) {
+
+    }
+}
+
+compChoice = getComputerChoice();
+console.log(compChoice);
+console.log(playRound("ROCk", compChoice));
